fb1466ac7bf91dbfb795e78fede6f3a01f2fca0e
Anurag Singh <tellanurag95@gmail.com>
2017-05-21 03:03:41 +0530

    Update application.js
    added depth

--- js/application.js
+++ js/application.js
@@ -1,4 +1,23 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+//0: up, 1:right,2 :down, 3:left
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  var game=new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  
+  function AI(){
+      setTimeout(function(){
+           var future=0;
+           if(game.grid.availableCells.length<5)
+               future=6;
+           else 
+             future=5;
+           if(game.move(game.getBestMove(game.grid,future))===false)
+           {
+             for(var i=0;i<4;i++)
+                 if(game.move(i)===true)
+                         break;
+           }
+           AI();
+      },50);
+  }
+   AI();
 });
